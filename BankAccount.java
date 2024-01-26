f20ecd50acc92cdd4b95a57ea7edbc6ecf8f9175
SanskarGandhewar <157237993+SanskarGandhewar@users.noreply.github.com>
2024-01-26 15:39:30 +0530

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,36 @@
+package codsoft;
+
+import java.util.Scanner;
+
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful. Your new balance is: $" + balance);
+        } else {
+            System.out.println("Invalid amount. Please enter a positive amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal successful. Your new balance is: $" + balance);
+        } else if (amount <= 0) {
+            System.out.println("Invalid amount. Please enter a positive amount.");
+        } else {
+            System.out.println("Insufficient funds. Your current balance is: $" + balance);
+        }
+    }
+}
+
